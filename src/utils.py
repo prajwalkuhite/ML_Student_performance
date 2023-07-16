e83abb3be1648d92e80b94f8fb309ae49d05fd02
Prajwal Kuhite <prajwalkuhite50@gmail.com>
2023-07-16 20:54:41 +0530

    Project Completed

--- src/utils.py
+++ src/utils.py
@@ -24,25 +24,50 @@ def save_object (file_path, obj):
 
 ## Evaluate_model
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
-        report ={}
+        report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            model.fit(X_train, y_train)  ## Train model [i]
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            y_train_pred = model.predict(X_train)  # Training prediction
-            y_test_pred = model.predict(X_test)   # Testing prediction
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
-            train_model_score = r2_score(y_train,y_train_pred)
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
 
         return report
-    
+
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
+    
+
+## For loading the objects
+
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
